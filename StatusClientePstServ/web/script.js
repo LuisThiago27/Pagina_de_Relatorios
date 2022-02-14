e21d6e753eff26239d11c824ad6be0bf281840c2
LuisThiago27 <87100996+LuisThiago27@users.noreply.github.com>
2022-02-14 14:58:54 -0300

    Add files via upload

--- StatusClientePstServ/web/script.js
+++ StatusClientePstServ/web/script.js
@@ -154,3 +154,12 @@ function validateForm() {
         return false;
     }
 }  
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
